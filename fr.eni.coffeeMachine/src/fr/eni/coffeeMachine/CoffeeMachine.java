3b209141623ffbef326e4c266e75c33fd5b152ef
adelie <adelie@mbp-de-adelie.home>
2021-03-22 22:23:36 +0100

    Corrections diverses, ajout d'un sysout conditionnel dans main, TODO

--- fr.eni.coffeeMachine/src/fr/eni/coffeeMachine/CoffeeMachine.java
+++ fr.eni.coffeeMachine/src/fr/eni/coffeeMachine/CoffeeMachine.java
@@ -8,23 +8,33 @@ public class CoffeeMachine {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		final int PRICE = 60;
+		int choice = 0;
 		int paid = 0;
-		int due = 0;
+		int changeDue = 0;
 		
 		do {
+			if(paid < PRICE) {
+				System.out.println("Vous avez mis " + (float)paid/100 + " euros. Il vous reste " + (float)(PRICE - paid)/100 + " euros à payer.");
+			}
 			showChoices(POSSIBLE_COINS);
-			paid = paid + POSSIBLE_COINS[scan.nextInt()-1];
+			choice = scan.nextInt();
+			paid = paid + POSSIBLE_COINS[choice - 1];
 		} while(paid < PRICE);
+		scan.close();
+		changeDue = paid - PRICE;
+		if(paid == PRICE){
+			System.out.println("Le compte est bon. Pas de monnaie à rendre. Voici votre café!");
+		} else {
+			System.out.println("Vous avez mis "+ (float)paid/100 + " euros. Je vous rends donc " + (float)changeDue/100 +" euros. Voici votre monnaie :");
+			showChange(changeDue);
+		}
 		
-		due = paid - PRICE;
-		System.out.println("Je vous rends donc " + due +"euros. Voici votre monnaie :");
-		showChange(due);
 		System.out.println("Merci pour votre achat! À bientôt!");
 	}
 
-	private static void showChange(int due) {
-		int[] numberOfCoins = numberOfCoins(due);
-		for(int i = 0; i<POSSIBLE_COINS.length; i++) {
+	private static void showChange(int changeDue) {
+		int[] numberOfCoins = numberOfCoins(changeDue);
+		for(int i = 0; i < POSSIBLE_COINS.length; i++) {
 			if(numberOfCoins[i] > 0) {
 				System.out.println(numberOfCoins[i] + " pièce(s) de " + (float)POSSIBLE_COINS[i]/100 + " euros");
 			}
@@ -32,13 +42,14 @@ public class CoffeeMachine {
 		
 	}
 
-	private static int[] numberOfCoins(int due) {
+	private static int[] numberOfCoins(int changeDue) {
 		int[] numberOfCoins = new int[POSSIBLE_COINS.length];
-		while(due >= 0){
-			for(int i = 0; i<POSSIBLE_COINS.length; i++) {
-				if(due <= POSSIBLE_COINS[i]) {
-					numberOfCoins[i] = due/POSSIBLE_COINS[i];
-					due -= due%POSSIBLE_COINS[i];
+		while(changeDue >= 0){
+			//TODO revoir ma logique mdr : trop de fatigue...
+			for(int i = 1; i < POSSIBLE_COINS.length; i++) {
+				if(changeDue <= POSSIBLE_COINS[i]) {
+					numberOfCoins[i-1] = POSSIBLE_COINS[i-1]/changeDue;
+					changeDue -= POSSIBLE_COINS[i-1]%changeDue;
 				} else {
 					numberOfCoins[i] = 0;
 				}
