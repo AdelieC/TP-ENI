1e9dd384ad2bca3d53d0b13c8f313581c2dac649
DidilY <castel.adelie@yahoo.com>
2021-03-22 16:35:09 +0100

    J'ai fini le tp de cryptage.

--- fr.eni.cryptage/src/fr/eni/cryptage/Cryptage.java
+++ fr.eni.cryptage/src/fr/eni/cryptage/Cryptage.java
@@ -17,27 +17,35 @@ public class Cryptage {
 		System.out.println("Saisissez une clé de moins de 9 lettres pour crypter votre phrase :");
 		key = scan.nextLine().toUpperCase();
 		} while(!key.matches("[A-Z]{1,8}"));
-				
+		
+		System.out.println("La phrase \"" + sentence + "\" cryptée avec la clé \"" + key + "\" est :");		
 		sentence = crypt(sentence, key);
 		System.out.println(sentence);
 		scan.close();
 	}
 
 	private static String crypt(String sentence, String key) {
-		String az = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		final String AZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		String fullKey = fitKeyToSentence(key, sentence);
 		String encrypted = "";
+		int correspondingIndex;
 		
-		//Il faut trouver le char de az dont l'index est égal à l'index d'az du char de sentence + l'index d'az du char de fullKey
 		for(int i = 0; i<sentence.length(); i++) {
 			//a) Checking if char at index i is a whitespace
 			if(sentence.charAt(i) == ' ') {
 				encrypted += " ";
+			//b) If not whitespace, finding index of AZ char to put in encrypted string
 			} else {
-				
+				correspondingIndex = AZ.indexOf(sentence.charAt(i)) + AZ.indexOf(fullKey.charAt(i));
+				//c) If corresponding index is within AZ range, we put corresponding char inside encrypted sentence
+				if(correspondingIndex < AZ.length()) {
+					encrypted += AZ.charAt(correspondingIndex);
+				//d) If corresponding index is greater than AZ's length, we remove AZ's length and start the alphabet again
+				} else {
+					encrypted += AZ.charAt(correspondingIndex - AZ.length());
+				}
 			}
 		}
-		
 		return encrypted;
 	}
 
@@ -65,5 +73,4 @@ public class Cryptage {
 		}
 		return fullKey;
 	}
-	
 }
