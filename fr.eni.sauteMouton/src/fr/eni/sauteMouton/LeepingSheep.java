968cdc0af643ca307c3feb548e33a67176220de4
DidilY <castel.adelie@yahoo.com>
2021-03-23 11:42:54 +0100

    Création d'une nouvelle méthode tryMove. Erreur scanner : TODO!!

--- fr.eni.sauteMouton/src/fr/eni/sauteMouton/LeepingSheep.java
+++ fr.eni.sauteMouton/src/fr/eni/sauteMouton/LeepingSheep.java
@@ -21,21 +21,57 @@ public class LeepingSheep {
 
 	private static void play(char[] gameBoard) {
 		int input;
+		Scanner scan = new Scanner(System.in);
 		
 		//1) Checking if the user won
 		if(won(gameBoard)) {
+			System.out.println("Bravo! Vous avez gagné!!");
 			
 		//2) Checking if the user is stuck
 		} else if(stuck(gameBoard)) {
-		
-		//3) Otherwise, enter continue recursion to play!
+			System.out.println("Vous avez bloqué vos moutons. Désolé mais vous avez perdu.");
+			
+		//3) Otherwise, enter or continue recursion to play!
 		} else {
+			//a) showing state of the game :
+			show(gameBoard);
 			
+			//b) asking user to choose a sheep to move
+			System.out.println("Choisissez un mouton à faire avancer. Entrez son numéro de case :");
+			input = scan.nextInt();
 			
-			
+			//c) checking if number chosen matches position of a sheep
+			if(input < 0 || input > gameBoard.length-1 || gameBoard[input] == FREE_SPOT) {
+				System.out.println("Il n'y a pas de mouton à cet endroit... Réessayez.");
+				
+			//d) moving or not moving sheep :
+			} else if(gameBoard[input] == LEAPING_LEFT) {
+				tryMove(gameBoard, input, true);
+			} else {
+				tryMove(gameBoard, input, false);
+			}
 			play(gameBoard);
 		}
-		show(gameBoard);
+		scan.close();
+	}
+
+	private static void tryMove(char[] gameBoard, int input, Boolean left) {
+		int end = left? 0 : gameBoard.length - 1;
+		int next = left? input - 1 : input + 1;
+		int jumpable = left? input - 2 : input + 2;
+		if(input == end) {
+			System.out.println("Impossible d'aller plus loin avec celui-là. Choisissez un autre mouton.");
+		} else if(gameBoard[next] == FREE_SPOT) {
+			gameBoard[next] = gameBoard[input];
+	        gameBoard[input] = FREE_SPOT;
+		} else {
+			if(gameBoard[jumpable] == FREE_SPOT) {
+				gameBoard[jumpable] = gameBoard[input];
+	            gameBoard[input] = FREE_SPOT;
+			} else {
+				System.out.println("Impossible de bouger ce mouton : il n'a pas la place d'avancer");
+			}
+		}
 	}
 
 	private static boolean stuck(char[] gameBoard) {
