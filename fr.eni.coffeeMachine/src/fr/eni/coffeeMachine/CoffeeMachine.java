d6413ffd3c32be54599bb5faa4b760b1d00baaaa
DidilY <castel.adelie@yahoo.com>
2021-03-23 09:06:21 +0100

    Ajouts de commentaires et amélioration de l'UX (si on peut appeler ça comme ça à ce stade haha)

--- fr.eni.coffeeMachine/src/fr/eni/coffeeMachine/CoffeeMachine.java
+++ fr.eni.coffeeMachine/src/fr/eni/coffeeMachine/CoffeeMachine.java
@@ -12,37 +12,45 @@ public class CoffeeMachine {
 		int paid = 0;
 		int changeDue = 0;
 		
+		//1) Loop to get coins input(s) from user
 		do {
+			System.out.println("Prix du café : " + (float)PRICE/100 + " euros");
 			if(paid < PRICE) {
 				System.out.println("Vous avez mis " + (float)paid/100 + " euros. Il vous reste " + (float)(PRICE - paid)/100 + " euros à payer.");
 			}
 			showChoices(POSSIBLE_COINS);
 			choice = scan.nextInt();
-			paid = paid + POSSIBLE_COINS[choice - 1];
+			if(choice > 0 && choice < 7) {
+				paid = paid + POSSIBLE_COINS[choice - 1];
+			} else {
+				System.out.println("Choix de pièce incorrect : entrez un chiffre compris entre 1 et 6");
+			}
 		} while(paid < PRICE);
-		scan.close();
+		
+		//2) If there's change to be given back, show infos + calculate change and show result
 		changeDue = paid - PRICE;
-		if(paid == PRICE){
+		if(changeDue == 0){
 			System.out.println("Le compte est bon. Pas de monnaie à rendre. Voici votre café!");
 		} else {
 			System.out.println("Vous avez mis "+ (float)paid/100 + " euros. Je vous rends donc " + (float)changeDue/100 +" euros. Voici votre monnaie :");
 			showChange(changeDue);
 		}
-		
 		System.out.println("Merci pour votre achat! À bientôt!");
+		scan.close();
 	}
 
 	private static void showChange(int changeDue) {
+		//Show number of coins (> 0) of each possible coin value by looping through both arrays
 		int[] numberOfCoins = numberOfCoins(changeDue);
 		for(int i = 0; i < POSSIBLE_COINS.length; i++) {
 			if(numberOfCoins[i] > 0) {
 				System.out.println(numberOfCoins[i] + " pièce(s) de " + (float)POSSIBLE_COINS[i]/100 + " euros");
 			}
 		}
-		
 	}
 
 	private static int[] numberOfCoins(int changeDue) {
+		//Put numbers of coins of each possible coin value into an array which is exactly the same length as POSSIBLE_COINS
 		int[] numberOfCoins = new int[POSSIBLE_COINS.length];
 		while(changeDue > 0){
 			for(int i = 0; i < POSSIBLE_COINS.length; i++) {
@@ -58,6 +66,7 @@ public class CoffeeMachine {
 	}
 
 	private static void showChoices(int[] POSSIBLE_COINS) {
+		//Loop through POSSIBLE_COINS array to show the list of possible choices for coins
 		System.out.println("Entrez une pièce dans la machine :");
 		for(int i = 0; i<POSSIBLE_COINS.length; i++) {
 			System.out.println((i+1) + " - Une pièce de " + (float)POSSIBLE_COINS[i]/100 + " euros");
--- fr.eni.melangeur/src/fr/eni/melangeur/Melangeur.java
+++ fr.eni.melangeur/src/fr/eni/melangeur/Melangeur.java
@@ -3,7 +3,6 @@ package fr.eni.melangeur;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class Melangeur {
 
 	public static void main(String[] args) {
@@ -11,8 +10,11 @@ public class Melangeur {
 		String[] input;
 		String newSentence = "";
 		
+		//1) Getting sentence to transform from user, putting it inside array of strings
 		System.out.println("Saisir la phrase que vous souhaitez transformer :");
 		input = scan.nextLine().split(" ");
+		
+		//2) Loop through the array, and if string is long enough to need mixing, get the middle, reverse it, and put everything back together again
 		for(int i = 0; i < input.length; i++) {
 			char[] word = input[i].toCharArray();
 			int taille = word.length;
@@ -23,11 +25,14 @@ public class Melangeur {
 			}
 			newSentence += " ";
 		}
-		System.out.println("La phrase transformée : " + newSentence);
+		
+		//3) Show result
+		System.out.println("Votre phrase était : " + input + ". La phrase transformée : " + newSentence);
 		scan.close();
 	}
 
 	private static String mixLetters(char[] word) {
+		//Taking in middle of a word (word param when method is called in main) as array, reversing it and giving the result back as a string
 		char temp;
 		final int LENGTH = word.length;
 		for(int i = 0; i < LENGTH / 2; i++) {
