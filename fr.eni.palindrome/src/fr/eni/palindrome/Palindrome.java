1fa3df077048c251c97e5eda83fd9b25f23e9db0
DidilY <castel.adelie@yahoo.com>
2021-03-23 15:57:33 +0100

    Ajout TP palindrome fini

---
+++ fr.eni.palindrome/src/fr/eni/palindrome/Palindrome.java
@@ -0,0 +1,31 @@
+package fr.eni.palindrome;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String word = "";
+		
+		//1) Ask the user for a word to validate as a palindrome or not
+		System.out.println("Bonjour! Vous voulez vérifier si un mot est un palindrome? Écrivez-le ici :");
+		word = scan.nextLine().toUpperCase();
+		if(isPalin(word)) {
+			System.out.println("Le mot " + word + " est bien un palindrome!");
+		} else {
+			System.out.println("Le mot " + word + " n'est pas un palindrome.");
+		}
+		scan.close();
+	}
+
+	private static boolean isPalin(String word) {
+		int i = 0;
+		boolean palin = false;
+		do {
+			palin = (word.charAt(i) == word.charAt(word.length()-1 -i));
+			i++;
+		} while (palin && i<word.length()/2);
+		return palin;
+	}
+}
