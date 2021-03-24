096f488cb112941e86cd851fa41b901e0b96fb5f
DidilY <castel.adelie@yahoo.com>
2021-03-24 18:25:13 +0100

    Ajout suiteLogique : rien codé encore dedans!

---
+++ fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
@@ -0,0 +1,15 @@
+package fr.eni.suiteLogique;
+
+public class Sequence {
+
+	public static void main(String[] args) {
+		final int DEPTH = 40;
+		char[] sequence = {1};
+		
+		//TODO : euh, bah tout
+		for(int i = 0, j = 0; i<DEPTH + 1; i++) {
+			
+		}
+	}
+
+}
