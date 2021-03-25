72d168e7d82bf1a31cc682b4bb2051fb5c283b15
DidilY <castel.adelie@yahoo.com>
2021-03-25 11:32:14 +0100

    Après moult tentatives et méthodes différentes, tjrs pas la logique...

--- fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
+++ fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
@@ -4,12 +4,38 @@ public class Sequence {
 
 	public static void main(String[] args) {
 		final int DEPTH = 40;
-		char[] sequence = {1};
+		int[] sequence = {1,2,1,1};
 		
-		//TODO : euh, bah tout
-		for(int i = 0, j = 0; i<DEPTH + 1; i++) {
-			
+		/* 1) Commençons par le commencement : on va déjà essayer de passer un niveau en commençant au milieu!
+		-> de 1211 à 111221 :
+		*/
+		for(int i = 0; i<10; i++) {
+			sequence = nextLevel(sequence);
+			show(sequence);
 		}
 	}
 
+	private static int[] nextLevel(int[] sequence) {
+		for(int i = 0, count = 0; i<sequence.length; i++) {
+			if(i>0) {
+				if(sequence[i-1] == sequence[i]) {
+					count++;
+					System.out.println("same digit and count = " + count);
+				} else {
+					count = 0;
+					System.out.println("not same digit and count = " + count);
+				}
+			} else {
+				
+			}
+		}
+		return sequence;
+	}
+
+	private static void show(int[] sequence) {
+		for(int digit : sequence) {
+			System.out.print(digit);
+		}
+		System.out.println();
+	}
 }
