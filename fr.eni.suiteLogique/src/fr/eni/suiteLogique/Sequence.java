22817b397b0cbb0090489425db6d551ab632994b
DidilY <castel.adelie@yahoo.com>
2021-03-25 12:40:33 +0100

    Ajout du minuteur qui me renvoie un 7s rageant!! Convert to char[]?

--- fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
+++ fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
@@ -3,13 +3,17 @@ package fr.eni.suiteLogique;
 public class Sequence {
 
 	public static void main(String[] args) {
-		final int DEPTH = 10;
+		final int DEPTH = 40;
 		String sequence = "1";
 		
-		for(int i = 0; i<DEPTH+1; i++) {
+		long start = System.nanoTime();
+		for(int i = 0; i<DEPTH; i++) {
 			System.out.println(sequence);
 			sequence = nextLevel(sequence) + ".";
 		}
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Durée d'exécution pour une profondeur de " + DEPTH + " = " + time/1000000 + " millisecondes");
 	}
 
 	private static String nextLevel(String sequence) {
