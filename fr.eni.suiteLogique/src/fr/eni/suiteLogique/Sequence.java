b672cd991f21653e44f6a0351d2c56d8824e03cf
DidilY <castel.adelie@yahoo.com>
2021-03-25 12:19:59 +0100

    Bon. suite logique marche enfiiiiin pour depth = 10. Sceptique pour 40.

--- fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
+++ fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
@@ -4,10 +4,8 @@ public class Sequence {
 
 	public static void main(String[] args) {
 		final int DEPTH = 10;
-		String sequence = "1211";
+		String sequence = "1";
 		
-		/* changement de type (ENCORE.......) -> on va bosser avec des strings!
-		*/
 		for(int i = 0; i<DEPTH+1; i++) {
 			System.out.println(sequence);
 			sequence = nextLevel(sequence) + ".";
@@ -15,16 +13,20 @@ public class Sequence {
 	}
 
 	private static String nextLevel(String sequence) {
-		String tmp = "";
-		for(int i = 1, count = 1; i<sequence.length(); i++) {
-			if(sequence.charAt(i) != sequence.charAt(i-1)) {
-				tmp += count + 0;
-				tmp += sequence.charAt(i-1);
-				count = 1;
-			} else {
-				count++;
+		String newSequence = "";
+		if(sequence != "1") {
+			for(int i = 1, count = 1; i<sequence.length(); i++) {
+				if(sequence.charAt(i) != sequence.charAt(i-1)) {
+					newSequence += count + 0;
+					newSequence += sequence.charAt(i-1);
+					count = 1;
+				} else {
+					count++;
+				}
 			}
+		} else {
+			newSequence = "11";
 		}
-		return tmp;
+		return newSequence;
 	}
 }
