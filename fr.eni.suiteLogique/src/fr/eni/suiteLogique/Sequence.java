27c63ba9194cac877cda671949eab49d1131001c
DidilY <castel.adelie@yahoo.com>
2021-03-25 12:07:41 +0100

    Il y a du progrès sur la suite!! halleluia!!

--- fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
+++ fr.eni.suiteLogique/src/fr/eni/suiteLogique/Sequence.java
@@ -3,39 +3,28 @@ package fr.eni.suiteLogique;
 public class Sequence {
 
 	public static void main(String[] args) {
-		final int DEPTH = 40;
-		int[] sequence = {1,2,1,1};
+		final int DEPTH = 10;
+		String sequence = "1211";
 		
-		/* 1) Commençons par le commencement : on va déjà essayer de passer un niveau en commençant au milieu!
-		-> de 1211 à 111221 :
+		/* changement de type (ENCORE.......) -> on va bosser avec des strings!
 		*/
-		for(int i = 0; i<10; i++) {
-			sequence = nextLevel(sequence);
-			show(sequence);
+		for(int i = 0; i<DEPTH+1; i++) {
+			System.out.println(sequence);
+			sequence = nextLevel(sequence) + ".";
 		}
 	}
 
-	private static int[] nextLevel(int[] sequence) {
-		for(int i = 0, count = 0; i<sequence.length; i++) {
-			if(i>0) {
-				if(sequence[i-1] == sequence[i]) {
-					count++;
-					System.out.println("same digit and count = " + count);
-				} else {
-					count = 0;
-					System.out.println("not same digit and count = " + count);
-				}
+	private static String nextLevel(String sequence) {
+		String tmp = "";
+		for(int i = 1, count = 1; i<sequence.length(); i++) {
+			if(sequence.charAt(i) != sequence.charAt(i-1)) {
+				tmp += count + 0;
+				tmp += sequence.charAt(i-1);
+				count = 1;
 			} else {
-				
+				count++;
 			}
 		}
-		return sequence;
-	}
-
-	private static void show(int[] sequence) {
-		for(int digit : sequence) {
-			System.out.print(digit);
-		}
-		System.out.println();
+		return tmp;
 	}
 }
