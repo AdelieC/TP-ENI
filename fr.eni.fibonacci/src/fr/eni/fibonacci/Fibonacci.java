d5f953caf866c39108209fdc06c2b4227c69c204
adelie <adelie@lubuntu-1.home>
2021-03-28 20:26:53 +0200

    Fibonacci : 2e méthode récursion ajoutée + temps

--- fr.eni.fibonacci/src/fr/eni/fibonacci/Fibonacci.java
+++ fr.eni.fibonacci/src/fr/eni/fibonacci/Fibonacci.java
@@ -4,27 +4,56 @@ import java.util.Scanner;
 
 public class Fibonacci {
 	public final static Scanner SC = new Scanner(System.in);
-	
 	/**
 	 * Different methods to get fibonacci sequence, with time of execution for each method
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		final int MAX = 92;
-		int length = 0;
-		
+		int length = 0, count = 0;
+		double start, end;
+		long a = 1, b = 1;
 		//1) Ask user for a number between 0 and max :
 		length = askForLength(MAX);
 		
 		//2) Show fib sequence until depth = length given by user
-		showFibonacci(length);
+		//a) Method 1 with for loop and long
+		start = System.nanoTime();
+		System.out.println("―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+		longFibonacci(length);
+		end = System.nanoTime();
+		System.out.println();
+		System.out.println("―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+		System.out.println("Temps d'exécution avec for loop et longs : " + (end - start) / 1000000 + " millisecondes");
+		System.out.println("―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+		System.out.println();
 		
-
+		//b) Method 2 with recursion and long (bigints = dumb choice cause they're not supposed to change values)
+		start = System.nanoTime();
+		System.out.print(0);
+		recursFibonacci(a, b, count, length);
+		end = System.nanoTime();
+		System.out.println();
+		System.out.println("―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
+		System.out.println("Temps d'exécution avec for loop et BigInts : " + (end - start) / 1000000 + " millisecondes");
+		System.out.println("―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――");
 	}
 
-	private static void showFibonacci(int length) {
+	private static void recursFibonacci(long a, long b, int count, int length) {
+		long tmp = 0;
+		System.out.print(", ");
+		if(count>9 && count%10 == 0) System.out.println();
+		System.out.print(a);
+		tmp = b;
+		b += a;
+		a = tmp;
+		count++;
+		if(count < length) recursFibonacci(a, b, count, length);
+	}
+
+	private static void longFibonacci(int length) {
+		long a = 1, b = 1, tmp = 0;
 		System.out.print(0);
-		long a=1, b=1, tmp=0;
 		for(int i = 0; i<length; i++) {
 			System.out.print(", ");
 			if(i>9 && i%10 == 0) System.out.println();
