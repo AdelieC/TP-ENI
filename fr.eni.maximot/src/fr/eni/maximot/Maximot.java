82c3d4c3fb1f5feebbdc263c56118dcc6f6f9a3c
DidilY <castel.adelie@yahoo.com>
2021-04-19 00:34:53 +0200

    TP module 1 POO terminé. TODO -> javadoc

--- fr.eni.maximot/src/fr/eni/maximot/Maximot.java
+++ fr.eni.maximot/src/fr/eni/maximot/Maximot.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class Maximot {
 	public final static Scanner SCAN = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		final String PATH = "dictionnaire.txt";
 		String word = "";
@@ -18,15 +19,30 @@ public class Maximot {
 		try {
 			setOfLetters = shuffleLettersIn(word);
 			input = askForInput(setOfLetters);
-			printResult(input, setOfLetters, word);
+			printResult(input, setOfLetters, word, PATH);
 		} catch (Exception e ) {
 			System.err.println(e.getMessage());
 		}
 		SCAN.close();
 	}
 	
-	private static void printResult(String input, String mixedWord, String word) {
-		
+	private static void printResult(String input, String mixedWord, String word, String path) {
+		boolean found = false;
+		if(input.compareTo(word) == 0) {
+			System.out.println("Félicitations! " + input + " est bien le plus long que vous puissiez former avec les lettres : " + mixedWord);
+		} else {
+			try(FileInputStream dictionnary = new FileInputStream(path)) {
+				Scanner reader = new Scanner(dictionnary);
+				while(reader.hasNextLine() && !found) {
+					found = (input.compareTo(reader.nextLine().toUpperCase()) == 0);
+				}
+				System.out.println(found ? "Le mot " + input + " n'était pas le mot le plus long, mais il existe tout de même! Bravo!" : "Désolé! " + input + " n'est pas dans le dico!");
+			} catch(FileNotFoundException e) {
+				System.err.println("Impossible de vérifier l'existence du mot : le fichier " + path + " n'a pas été trouvé.");
+			} catch(IOException e) {
+				System.err.println("Impossible de vérifier l'existence du mot : le fichier " + path + " n'a pas pu être lu.");
+			}
+		}
 	}
 	
 	private static String askForInput(String setOfLetters) {
@@ -40,33 +56,34 @@ public class Maximot {
 			System.out.println("Écrivez un mot uniquement avec les lettres que vous avez tirées :");
 			input = SCAN.nextLine();
 		} while(!foundInSet(setOfLetters,input));
-		return null;
+		return input.toUpperCase();
 	}
 	
 	private static boolean foundInSet(String setOfLetters, String input) {
 		boolean found = false;
 		int i = 0;
 		do {
-			found = (setOfLetters.indexOf(input.charAt(i)) >= 0);
+			found = (setOfLetters.indexOf(input.toUpperCase().charAt(i)) >= 0);
 			i++;
 		} while(found && i<input.length());
 		return found;
 	}
 
 	private static String shuffleLettersIn(String word) throws Exception {
-		if(!word.isBlank()) throw new Exception("Erreur: le mot généré à partir du dictionnaire est vide. Impossible de lancer le jeu.");
-
-		char letters[] = word.toCharArray();
-		char temp;
-		Random rand = new Random();
+		if(word.isBlank()) throw new Exception("Erreur: le mot généré à partir du dictionnaire est vide. Impossible de lancer le jeu.");
+		else {
+			char letters[] = word.toCharArray();
+			char temp;
+			Random rand = new Random();
 		
-		for(int i = 0, j = 0; i<letters.length; i++) {
-			j = rand.nextInt(letters.length);
-			temp = letters[i];
-			letters[i] = letters[j];
-			letters[j] = temp;
+			for(int i = 0, j = 0; i<letters.length; i++) {
+				j = rand.nextInt(letters.length);
+				temp = letters[i];
+				letters[i] = letters[j];
+				letters[j] = temp;
+			}
+			return String.valueOf(letters);
 		}
-		return String.valueOf(letters);
 	}
 	
 	private static String getRandomWord(String path) {
