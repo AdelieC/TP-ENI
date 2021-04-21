664e0b949d66ba846b405fded94e98cd257c18ba
DidilY <castel.adelie@yahoo.com>
2021-04-21 09:51:17 +0200

    Calendrier fini et javadé/docké? javadocké c'est mieux.

--- fr.eni.calendrier/src/fr/eni/calendar/Calendar.java
+++ fr.eni.calendrier/src/fr/eni/calendar/Calendar.java
@@ -2,9 +2,16 @@ package fr.eni.calendar;
 
 import java.util.GregorianCalendar;
 import java.util.Locale;
+import java.util.Scanner;
 
+/**
+ * Shows the calendar of the current month in the console by default.
+ * You can choose another year and month if you follow the instructions at the bottom of the calendar.
+ * @author adelie
+ *
+ */
 public class Calendar {
-
+	public final static Scanner SCAN = new Scanner(System.in);
 	public static void main(String[] args) {
 		GregorianCalendar cal = new GregorianCalendar(Locale.FRANCE);
 		
@@ -13,21 +20,77 @@ public class Calendar {
 			cal = changeCalendar(cal);
 			showCalendar(cal);
 		}
-
+		System.out.println("**************************************************");
+		System.out.println("Vous avez choisi de quitter. Au revoir et à bientôt!");
+		System.out.println("**************************************************");
 	}
 
 	
+	/**
+	 * Changes the year and month of the calendar with values given as input by the user.
+	 * @param GregorianCalendar cal
+	 * @return GregorianCalendar cal
+	 */
 	private static GregorianCalendar changeCalendar(GregorianCalendar cal) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private static boolean askSomethingElse() {
-		// TODO Auto-generated method stub
-		return false;
+		String year = "", month = "";
+		boolean valid = false;
+		do {
+				System.out.println("Entrez l'année que vous souhaitez voir :");
+				year = SCAN.nextLine();
+				valid = validateInt(year);
+		} while(!valid);
+		do {
+				System.out.println("Entrez le numéro du mois que vous souhaitez voir :");
+				month = SCAN.nextLine();
+				valid = validateInt(month);
+		} while(!valid);
+		
+		cal.set(GregorianCalendar.YEAR, Integer.parseInt(year));
+		cal.set(GregorianCalendar.MONTH, Integer.parseInt(month)-1);
+		return cal;
 	}
 	
+	/**
+	 * Checks if a String value can be converted into an int.
+	 * Useful to check the validity of a supposed number given as input by a user.
+	 * @param String number to convert into int
+	 * @return boolean set to true if number is convertible to an int
+	 */
+	private static boolean validateInt(String num) {
+		boolean valid = false;
+		try {
+			Integer.parseInt(num);
+			valid = true;
+		} catch(Exception e) {
+			System.out.println();
+			System.err.println("La valeur entrée est invalide. Entrez une valeur valide!");
+			System.out.println();
+		}
+		return valid;
+	}
+
+	/**
+	 * Prints a menu in the console to ask the user if he wants to do something else or to quit.
+	 * @return boolean set to true if the user wants to do something else.
+	 */
+	private static boolean askSomethingElse() {
+		boolean changeCal = false;
+		System.out.println("Tapez 1 pour voir un autre mois, 2 pour quitter :");
+		changeCal = SCAN.nextLine().equals("1");
+		return changeCal;
+	}
+	
+	/**
+	 * Prints a little calendar in the console showing the month of the GregorianCalendar value given as param.
+	 * @param GregorianCalendar cal
+	 */
 	private static void showCalendar(GregorianCalendar cal) {
+		int day = 0, gap = 0, max = 0;
+		
+		//1) Reset date to first day of the month :
+		cal.set(GregorianCalendar.DAY_OF_MONTH, 1);
+		
+		//2) Print display
 		System.out.println("――――――――――――");
 		System.out.println("CALENDRIER");
 		System.out.println("――――――――――――");
@@ -39,9 +102,26 @@ public class Calendar {
 				cal.get(GregorianCalendar.YEAR) +
 				"   *"
 		);
-		System.out.println(GregorianCalendar.);
-		System.out.println("jour : " + cal.getDisplayName(GregorianCalendar.DAY_OF_WEEK, GregorianCalendar.SHORT, Locale.FRANCE));
+		System.out.println("L  M  M  J  V  S  D");
+		
+		//3) Print the gap at the beginning :
+		day = cal.get(GregorianCalendar.DAY_OF_WEEK);
+		gap = (day == 1) ? 6: day-2;
+		for(int i = 0; i<gap; i++) System.out.print("   ");
+		
+		//4) Find number of days in current month :
+		max = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+		
+		//5) Prints numbers and with all the returns in all the right places
+		for(int i = 1; i<=max; i++) {
+			if(i == 7-gap || (i+gap)%7 == 0) {
+				System.out.println(i);
+			} else {
+				System.out.print(i + " ");
+				if(i<=9)System.out.print(" ");
+			}
+		}
+		System.out.println();
+		System.out.println();
 	}
-
-
 }
